9ecbc18fed3c81aea6d29b3e5e9d19f6f7e2b5bb
DommSite <johan.johansson06@live.se>
2023-10-22 12:44:30 +0200

    Add project files.

---
+++ programmeringsuppgift4.7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace inlämningsuppgift4_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett flerordat meddelande");
+            string meddelande = Console.ReadLine();
+            Console.WriteLine("Hur många steg åt höger vill du ha dina meddelanden");
+            int steg = int.Parse(Console.ReadLine());
+            for (int i = 0; i < meddelande.Length; i++)
+            {
+                string bokstav = meddelande[i].ToString();
+                for (int j = 0; j < steg; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine(bokstav);
+            }
+            Console.ReadKey();
+        }
+    }
+}
